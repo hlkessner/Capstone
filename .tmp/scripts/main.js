69b21d0375ed18798da3d2bf9e97e7caaaaaaec6
Halsey Kessner <kessnerh@seattleu.edu>
2017-02-09 18:21:43 -0800

    initial commit

---
+++ .tmp/scripts/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6
+  });
+  var infoWindow = new google.maps.InfoWindow({ map: map });
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+}
+$.getJSON('http://api.petfinder.com/pet.find?format=json&key=12ca8a73b3b4a51797988576ecfd5ebc&callback=?', function (data) {
+  location: 98116;
+  console.log(data);
+});
+//# sourceMappingURL=main.js.map
