c43c5b401e213ac7c65120253522f32fd7435037
Halsey Kessner <kessnerh@seattleu.edu>
2017-02-12 21:43:00 -0800

    added petfinder API

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+
+
 function initMap() {
        var map = new google.maps.Map(document.getElementById('map'), {
          zoom: 6
@@ -30,11 +32,42 @@ function initMap() {
                              'Error: The Geolocation service failed.' :
                              'Error: Your browser doesn\'t support geolocation.');
      }
-     $.getJSON(
-         'http://api.petfinder.com/pet.find?format=json&key=12ca8a73b3b4a51797988576ecfd5ebc&callback=?',
-         function(data) {
-            location: 98116;
-            console.log(data);
-         }
 
-     );
+
+       //var url = 'http://api.petfinder.com/pet.find?format=json&key=12ca8a73b3b4a51797988576ecfd5ebc&location=98116&breed=cat&callback=?';
+          $.ajax({
+              type : 'GET',
+              data : {},
+              url : 'http://api.petfinder.com/pet.find?format=json&key=12ca8a73b3b4a51797988576ecfd5ebc&location=98116&breed=cat&callback=?',
+              dataType: 'json',
+              success : function(data) {
+                  // stores result
+                  var result = '';
+
+                  var petfinder = data.petfinder.$t;
+                  var infoHTML = '<ul>';
+                  infoHTML += '<li>';
+                  //pet description
+                  infoHTML += petfinder.pets.pet.name.$t;
+                  infoHTML += '</li>';
+                  //shows pet's image
+                  infoHTML += '<li>';
+                  $.each(petfinder.pet.media.photos.photo, function(i, image){
+                      if(image['@size'] == 'x' && image['@id'] === '1'){
+                          infoHTML += '<img src="'+image['$t']+'" />';
+                      }
+                      if (image == "undefined"){
+                        infoHTML += 'Cat not found. Please refresh page';
+                      }
+                  });
+                  infoHTML += '</li>';
+                  infoHTML += '</ul>';
+                  // return infoHTML;
+                  $('#petfinderInfo').html(infoHTML);
+                  console.log(petfinder);
+              },
+              error : function(request,error)
+              {
+                  alert("No Cats Found. Please refresh page");
+              }
+          });
