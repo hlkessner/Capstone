8053a494db85636ddf57e8b5fb7a2060b463fc2a
Halsey Kessner <kessnerh@seattleu.edu>
2017-02-19 12:18:15 -0800

    PetFiner API working and rescues maping

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,12 +1,14 @@
 'use strict';
 
+var map;
+var infowindow;
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12
   });
   var infoWindow = new google.maps.InfoWindow({ map: map });
-
-  // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
       var pos = {
@@ -15,7 +17,7 @@ function initMap() {
       };
 
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setContent('You are here!');
       map.setCenter(pos);
     }, function () {
       handleLocationError(true, infoWindow, map.getCenter());
@@ -24,14 +26,42 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+  }
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  var myLatLng = { lat: 47.620422, lng: -122.349358 };
+  service.nearbySearch({
+    location: myLatLng,
+    radius: 1000000,
+    type: ['pet_store'],
+    keyword: 'rescue'
+  }, callback);
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
 }
 
-//var url = 'http://api.petfinder.com/pet.find?format=json&key=12ca8a73b3b4a51797988576ecfd5ebc&location=98116&breed=cat&callback=?';
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
 $.ajax({
   type: 'GET',
   data: {},
@@ -41,23 +71,27 @@ $.ajax({
     // stores result
     var result = '';
 
-    var petfinder = data.petfinder.$t;
+    var petfinder = data.petfinder.pets.pet;
     var infoHTML = '<ul>';
-    infoHTML += '<li>';
-    //pet description
-    infoHTML += petfinder.pets.pet.name.$t;
-    infoHTML += '</li>';
-    //shows pet's image
-    infoHTML += '<li>';
-    $.each(petfinder.pet.media.photos.photo, function (i, image) {
-      if (image['@size'] == 'x' && image['@id'] === '1') {
-        infoHTML += '<img src="' + image['$t'] + '" />';
-      }
-      if (image == "undefined") {
-        infoHTML += 'Cat not found. Please refresh page';
-      }
+    $.each(petfinder, function (i, pet) {
+
+      infoHTML += '<li>';
+      //pet name
+      infoHTML += pet.name.$t;
+      infoHTML += '</li>';
+      //shows pet's image
+      infoHTML += '<li>';
+      $.each(pet.media.photos.photo, function (y, image) {
+        if (image['@size'] == 'x' && image['@id'] === '1') {
+          infoHTML += '<img src="' + image['$t'] + '" />';
+        }
+        if (image == "undefined") {
+          infoHTML += 'Cat not found. Please refresh page';
+        }
+      });
+      infoHTML += '</li>';
     });
-    infoHTML += '</li>';
+
     infoHTML += '</ul>';
     // return infoHTML;
     $('#petfinderInfo').html(infoHTML);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,68 @@
+var map;
+      var infowindow;
+
+      function initMap() {
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+        if (navigator.geolocation) {
+                  navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = {
+                      lat: position.coords.latitude,
+                      lng: position.coords.longitude
+                    };
+
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent('You are here!');
+                    map.setCenter(pos);
+                  }, function() {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                  });
+                } else {
+                  // Browser doesn't support Geolocation
+                  handleLocationError(false, infoWindow, map.getCenter());
+                }
 
 
-function initMap() {
-       var map = new google.maps.Map(document.getElementById('map'), {
-         zoom: 6
-       });
-       var infoWindow = new google.maps.InfoWindow({map: map});
+              function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+                infoWindow.setPosition(pos);
+                infoWindow.setContent(browserHasGeolocation ?
+                                      'Error: The Geolocation service failed.' :
+                                      'Error: Your browser doesn\'t support geolocation.');
+              }
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        var myLatLng = {lat: 47.620422, lng: -122.349358};
+        service.nearbySearch({
+          location: myLatLng,
+          radius: 1000000,
+          type: ['pet_store'],
+          keyword: 'rescue'
+        }, callback);
+      }
 
-       // Try HTML5 geolocation.
-       if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(function(position) {
-           var pos = {
-             lat: position.coords.latitude,
-             lng: position.coords.longitude
-           };
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
+        }
+      }
 
-           infoWindow.setPosition(pos);
-           infoWindow.setContent('Location found.');
-           map.setCenter(pos);
-         }, function() {
-           handleLocationError(true, infoWindow, map.getCenter());
-         });
-       } else {
-         // Browser doesn't support Geolocation
-         handleLocationError(false, infoWindow, map.getCenter());
-       }
-     }
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
 
-     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-       infoWindow.setPosition(pos);
-       infoWindow.setContent(browserHasGeolocation ?
-                             'Error: The Geolocation service failed.' :
-                             'Error: Your browser doesn\'t support geolocation.');
-     }
-
-
-       //var url = 'http://api.petfinder.com/pet.find?format=json&key=12ca8a73b3b4a51797988576ecfd5ebc&location=98116&breed=cat&callback=?';
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
           $.ajax({
               type : 'GET',
               data : {},
@@ -44,23 +72,28 @@ function initMap() {
                   // stores result
                   var result = '';
 
-                  var petfinder = data.petfinder.$t;
+                  var petfinder = data.petfinder.pets.pet;
                   var infoHTML = '<ul>';
-                  infoHTML += '<li>';
-                  //pet description
-                  infoHTML += petfinder.pets.pet.name.$t;
-                  infoHTML += '</li>';
-                  //shows pet's image
-                  infoHTML += '<li>';
-                  $.each(petfinder.pet.media.photos.photo, function(i, image){
+                  $.each(petfinder, function(i, pet){
+
+                    infoHTML += '<li>';
+                    //pet name
+                    infoHTML += pet.name.$t;
+                    infoHTML += '</li>';
+                    //shows pet's image
+                    infoHTML += '<li>';
+                  $.each(pet.media.photos.photo, function(y, image){
                       if(image['@size'] == 'x' && image['@id'] === '1'){
                           infoHTML += '<img src="'+image['$t']+'" />';
                       }
                       if (image == "undefined"){
                         infoHTML += 'Cat not found. Please refresh page';
                       }
+                    });
+                      infoHTML += '</li>';
+
                   });
-                  infoHTML += '</li>';
+
                   infoHTML += '</ul>';
                   // return infoHTML;
                   $('#petfinderInfo').html(infoHTML);
