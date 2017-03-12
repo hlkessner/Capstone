4a1c092cbe2b0265417502e32d090ba179842076
Halsey Kessner <kessnerh@seattleu.edu>
2017-03-12 12:23:30 -0700

    Finished CatMatcher

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,11 +7,8 @@
     success: function(data) {
       // stores result
       var result = '';
-
       var petfinder = data.petfinder;
       var infoHTML = '<ul>';
-
-
         infoHTML += '<li id="name">';
         //pet name
         infoHTML += data.petfinder.pet.name.$t;
@@ -33,7 +30,10 @@
         infoHTML += '<button><a href=mailto:';
         //pet name
         infoHTML += data.petfinder.pet.contact.email.$t;
-        infoHTML += '>Adopt Me!</a></button>';
+        infoHTML += '?subject=Adoption%20Inquiry&body=Hello%20there!%0D%0A%0D%0AI%20am%20interested%20in%20adopting%20'
+        infoHTML += data.petfinder.pet.name.$t
+        infoHTML += '%20after%20finding%20him%20on%20CatMatcher.%20I%20can%20give%20this%20cat%20a%20great%20home!%20Please%20see%20my%20information%20below:>Adopt Me!</a></button>';
+        infoHTML += '<button><a href=https://secure3.convio.net/shs/site/Donation2;jsessionid=00000000.app326b?idb=143004535&1380.donation=form1&DONATION_LEVEL_ID_SELECTED=1&df_id=1380&mfc_pref=T&NONCE_TOKEN=BF7733985D5AC855261FCF38D05CD9D9&idb=0>Not ready to commit?</a></button>'
 
       infoHTML += '</ul>';
       // return infoHTML;
@@ -49,3 +49,14 @@
 
     }
   });
+  $('#form').validate({
+     submitHandler:function(form){
+       form.submit();
+     },
+     rules: {
+       "firstname": {
+         required: true,
+         maxlength: 128
+       }
+     }
+   });
